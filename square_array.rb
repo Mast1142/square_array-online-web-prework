bf2384404e18044ff6c5ec8bb6aaa160983ed608
Samuel W Tong <ssamt1142@gmail.com>
2019-02-21 03:47:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-array = [1, 2, 3]
-def square_array(array)
-  array.each do |n|
-    square = array.each{n**2}
+numbers = [1, 2, 3]
+def square_array(numbers)
+  numbers.each do |n|
+    square = numbers.each{n**2}
     puts square
   end
 end
